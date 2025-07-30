2665e0bae530b74eb31e88fcc211bdcecba1eb37
SuraKT <danhpham32122@gmail.com>
2025-07-30 15:19:45 +0700

    Create Main.lua

---
+++ TongHop/Main.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 126884695634066 then -- GAG
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SuraKTOnTop/Script/refs/heads/main/GAG.lua"))()
+elseif game.PlaceId == 2753915549 then -- Blox Fruits
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SuraKTOnTop/Script/refs/heads/main/Blox%20Fruits.lua"))()
+else
+end
